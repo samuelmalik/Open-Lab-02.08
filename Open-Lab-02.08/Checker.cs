2cc2fb31b506bf22612baa4af2c5254d7e5573b9
samuelmalik <samuelmalik9@gmail.com>
2022-09-20 09:57:28 +0200

    added code returning if string is empty or not

--- Open-Lab-02.08/Checker.cs
+++ Open-Lab-02.08/Checker.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_02._08
     {
         public bool IsEmpty(string str)
         {
-            throw new NotImplementedException();
+            return str.Length == 0;
         }
     }
 }
